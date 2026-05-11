5861062e7060a9f430a523bfdcb7655439159791
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2026-05-11 14:33:32 +0000

    generic chart done

--- 2-helm/deploy.sh
+++ 2-helm/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-helm upgrade --install hasher ./generic --values hasher.yaml
-helm upgrade --install redis ./generic --values redis.yaml
-helm upgrade --install rng ./generic --values rng.yaml
-helm upgrade --install webui ./generic --values webui.yaml
-helm upgrade --install worker ./generic --values worker.yaml
+set -eu
+for C in hasher redis rng webui worker; do
+  helm upgrade --install $C ./generic --values $C.yaml
+done
+
